27d847ba0d440e31c0f679129a897e58387c25af
unknown <alibhai93401@gmail.com>
2019-04-21 22:37:33 +0500

    initial commit

---
+++ hello.js
@@ -0,0 +1,26 @@
+var http = require('http');
+var fs = require('fs');
+var url = require('url');
+
+http.createServer(function (req,res) {
+	var u = url.parse(req.url , true);
+	console.log(u);
+	var filename = "." + u.pathname;
+	if(filename == './') { filename = './index'; }
+	filename = filename + ".html";
+	console.log(filename);
+	fs.readFile(filename , function(err,data) {
+		if(err){
+			res.writeHead(404 , {'Content-Type': 'text/html'});
+			return res.end('404 Not Found');
+		}
+		res.writeHead(200 , {'Content-Type': 'text/html'});
+	console.log('...Incoming Request: ' + "" + req.url);
+	res.write(data);
+	res.end();
+	})
+}).listen(8080);
+
+
+// var q = url.parse(req.url , true).query;
+	// var dates = q.year + " " + q.month;	
